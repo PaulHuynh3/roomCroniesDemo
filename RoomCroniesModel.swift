4ae99a6f2e2935126549152e0ef427e6dc8689b5
Paul <paul.huynh3@gmail.com>
2017-10-23 14:25:45 -0400

    delete

--- RoomCroniesModel.swift
+++ RoomCroniesModel.swift
@@ -14,13 +14,6 @@ class RoomCroniesModel: PFObject {
     override init() {
         super.init()
 
-        
-        //hey jaison
-<<<<<<< HEAD
-=======
-        //hey paul
->>>>>>> 8d697e96e12ec9d2adf5b56034fcbbf63bb7ce45
-        
     }
     
     
