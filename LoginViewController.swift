e02f07abf7c8f91f49c7cf054b2941f287d84a6e
Paul <paul.huynh3@gmail.com>
2017-10-24 18:53:31 -0400

    link the ibactions with the loginvc

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: IBActions
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+        
     }
+    
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+
     
 
     /*
