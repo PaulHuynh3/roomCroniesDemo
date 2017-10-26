1dee149256f67713da8753f0f730bbf25ad8ff4a
Paul <paul.huynh3@gmail.com>
2017-10-26 12:55:15 -0400

    fetch rooms in registervc to allow users to sign in to existing room

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -23,6 +23,7 @@ class RegisterViewController: UIViewController {
     
     
     var createRoom: Room?
+    var listOfRoom: [Room]?
 
     //MARK: View Did Load
     override func viewDidLoad() {
@@ -53,6 +54,7 @@ class RegisterViewController: UIViewController {
             self.performSegue(withIdentifier: "TaskViewControllerSegue", sender: nil)
         }
     }
+
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -62,16 +64,18 @@ class RegisterViewController: UIViewController {
             fatalError("unexpected destination:\(segue.destination)")
         }
         //creates a new room
-        createRoom = Room(roomName: existingRoomID.text!)
+        createRoom = Room(roomName: existingRoomTextField.text!)
         taskViewController.myRoom = createRoom
         
         //create user with 
-        guard let user = PFUser.current() else{
+        guard let user = PFUser.current() else {
             print("Error creating current user.")
             return
         }
+        
         taskViewController.myRoom?.roomCreator = user
         taskViewController.myRoom?.members.append(user)
+
         
         createRoom?.saveInBackground { (success: Bool?, error: Error?) in
             print(#line, success)
@@ -84,9 +88,30 @@ class RegisterViewController: UIViewController {
 //Fetch Parse
     
     func fetchExistingRoom () {
-    
-    
+        let query = PFQuery(className: "Room")
+        
+        query.findObjectsInBackground { (rooms:[PFObject]?, error: Error?) in
+            
+            if let error = error{
+                print(#line, error.localizedDescription)
+                return
+            }
+            
+            guard let rooms = rooms as? [Room] else {
+                return
+            }
+            
+            self.listOfRoom = rooms
+            
+        }
+        
     }
+    
+    
+    
+    
+    
+    
     
     
 }
