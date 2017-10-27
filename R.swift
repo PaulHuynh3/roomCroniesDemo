aa989dea79a738d2f087ea7042be4c2884fa1a89
Paul <paul.huynh3@gmail.com>
2017-10-26 21:49:54 -0400

    add helper class

--- R.swift
+++ R.swift
@@ -11,12 +11,10 @@ import Foundation
 // MARK: - Resources
 
 class R {
-    static let wallPicturesTableViewController = "WallPicturesTableViewController"
     
     static func error(with message: String) -> Error {
         let error = NSError(domain: "Custom", code: 100, userInfo: ["error" : message]) as Error
         return error
     }
     
-    static let wallPost = "WallPost"
 }
