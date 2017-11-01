f80fc8298555082b7347c3a42e084bd83e30d9ce
Paul <paul.huynh3@gmail.com>
2017-11-01 16:58:00 -0400

    completed by user works

--- TaskViewCell.swift
+++ TaskViewCell.swift
@@ -57,7 +57,7 @@ class TaskViewCell: UITableViewCell {
         if task.doneByUsername == nil {
             completedByLabel.text = "Completed by: 🤷‍♀️ 🤷‍♂️"
         } else {
-        completedByLabel.text = "Completed by"(task.doneByUsername)
+            completedByLabel.text = "Completed by: " + task.doneByUsername!
         }
     }
     
